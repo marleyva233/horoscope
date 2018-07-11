e159a184643af736ca13941501575212a16a2c95
Maribel <marleyva233@gmail.com>
2018-07-10 17:19:11 -0700

    added pictures and fixed typos

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 // i am creating an array to later access
 //i have created 12 objects, each one containing a different sign
 var zodiacSigns=[
-	{
+	{	
+		img: "img/newaries.png",
 		sign: "Aries",
 		positiveTraits:"Courageous, determined, confident, enthusiastic, optimistic, honest, passionate.",
 		negativeTraits: "Impatient, moody, short-tempered, impulsive, aggressive.",
@@ -12,6 +13,7 @@ var zodiacSigns=[
 		luckyDay: "Tuesday" ,
 	},
 	{
+		img: "img/newtaurus.png",
 		sign: "Taurus",
 		positiveTraits: "Reliable, patient, practical, devoted, responsible, stable.",
 		negativeTraits: "Stubborn, possessive, uncompromising.",
@@ -19,6 +21,7 @@ var zodiacSigns=[
 		luckyDay: "Friday and Monday",
 	},
 	{
+		img: "img/newgemini.png",
 		sign: "Gemini",
 		positiveTraits: "Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas",
 		negativeTraits: "Nervous, inconsistent,  and indecisive.",
@@ -26,6 +29,7 @@ var zodiacSigns=[
 		luckyDay: "Wednesday",
 	},
 	{
+		img: "img/newcancer.png",
 		sign: "Cancer",
 		positiveTraits: "Tenacius, highly imaginative, loyal, emotional, sympathetic, persuasive.",
 		negativeTraits: "Moody, pessimistic, suspicious, manipulative, insecure.",
@@ -33,6 +37,7 @@ var zodiacSigns=[
 		luckyDay: "Monday and Thursday.",
 	},
 	{
+		img: "img/newleo.png",
 		sign: "Leo",
 		positiveTraits: "Creative, passionate, generous, warm-hearted, cheerful, humorous.",
 		negativeTraits: "Arrogant, stubborn, self-centered, lazy, inflexible",
@@ -40,6 +45,7 @@ var zodiacSigns=[
 		luckyDay: "Sunday",
 	},
 	{
+		img: "img/newvirgo.png",
 		sign: "Virgo",
 		positiveTraits: "Loyal, analytical, kind, hardworking, practical.",
 		negativeTraits: "Shyness, worry, overly critical of self and others, all work and no play.",
@@ -47,6 +53,7 @@ var zodiacSigns=[
 		luckyDay: "Wednesday",
 	},
 	{
+		img: "img/newlibra.png",
 		sign: "Libra",
 		positiveTraits: "Cooperative, diplomatic, gracious, fair-minded, social.",
 		negativeTraits: "indecisive, avoids confrontations, will carry a grudge, self-pity.",
@@ -54,6 +61,7 @@ var zodiacSigns=[
 		luckyDay: "Friday",
 	},
 	{
+		img: "img/newscorpio.png",
 		sign: "Scorpio",
 		positiveTraits: "Resourceful, brave, passionate, stubborn, a true friend.",
 		negativeTraits: "Distrusting, jealous, secretive, violent.",
@@ -61,6 +69,7 @@ var zodiacSigns=[
 		luckyDay: "Tuesday",
 	},
 	{
+		img: "img/newsagittarius.png",
 		sign: "Sagittarius",
 		positiveTraits: "Generous, idealistic, great sense of humour.",
 		negativeTraits: "Promises more than can deliver, very impatient, will say anything no matter how undiplomatic.",
@@ -68,6 +77,7 @@ var zodiacSigns=[
 		luckyDay: "Thursday",
 	},
 	{
+		img: "img/newcapricorn.png",
 		sign: "Capricorn",
 		positiveTraits: "responsible, disciplined, self-control, good manners.",
 		negativeTraits: "Know-it-all, unforgiving, condescending, expecting the worst.",
@@ -75,6 +85,7 @@ var zodiacSigns=[
 		luckyDay: "Saturday",
 	},
 	{
+		img: "img/newaquarius.png",
 		sign: "Aquarius",
 		positiveTraits: "Progressie, original, independent, humanitarian.",
 		negativeTraits:"Runs from emotional expression, temperamental, uncompromising, aloof.",
@@ -82,6 +93,7 @@ var zodiacSigns=[
 		luckyDay: "Saturday",
 	},
 	{
+		img: "img/newpisces.png",
 		sign: "Pisces",
 		positiveTraits: "Compassionate, artistic, intuitive, gentle, wise, musical.",
 		negativeTraits: "Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr.",
@@ -111,72 +123,84 @@ function myZodiacSign(){
 	var mySign=mySign.toLowerCase();
 	console.log(mySign);
 	if (mySign=="aries") {
+		document.getElementById("crystalball").src=zodiacSigns[0].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[0].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[0].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[0].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[0].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[0].luckyDay;
 	} else if(mySign=="taurus"){
+		document.getElementById("crystalball").src=zodiacSigns[1].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[1].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[1].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[1].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[1].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[1].luckyDay;
 	}else if(mySign=="gemini"){
+		document.getElementById("crystalball").src=zodiacSigns[2].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[2].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[2].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[2].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[2].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[2].luckyDay;
 	}else if(mySign=="cancer"){
+		document.getElementById("crystalball").src=zodiacSigns[3].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[3].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[3].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[3].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[3].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[3].luckyDay;
 	}else if(mySign=="leo"){
+		document.getElementById("crystalball").src=zodiacSigns[4].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[4].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[4].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[4].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[4].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[4].luckyDay;
 	}else if(mySign=="virgo"){
+		document.getElementById("crystalball").src=zodiacSigns[5].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[5].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[5].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[5].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[5].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[5].luckyDay;
 	}else if(mySign=="libra"){
+		document.getElementById("crystalball").src=zodiacSigns[6].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[6].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[6].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[6].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[6].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[6].luckyDay;
 	}else if(mySign=="scorpio"){
+		document.getElementById("crystalball").src=zodiacSigns[7].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[7].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[7].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[7].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[7].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[7].luckyDay;
 	}else if(mySign=="sagittarius"){
+		document.getElementById("crystalball").src=zodiacSigns[8].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[8].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[8].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[8].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[8].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[8].luckyDay;
 	}else if(mySign=="capricorn"){
+		document.getElementById("crystalball").src=zodiacSigns[9].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[9].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[9].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[9].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[9].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[9].luckyDay;
 	}else if(mySign=="aquarius"){
+		document.getElementById("crystalball").src=zodiacSigns[10].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[10].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[10].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[10].negativeTraits;
 		document.getElementById("myLoveMatch").textContent="You get along bests with: "+ zodiacSigns[10].loveMatch;
 		document.getElementById("myLuckyDay").textContent="Your lucky day is: " +zodiacSigns[10].luckyDay;
 	}else if(mySign=="pisces"){
+		document.getElementById("crystalball").src=zodiacSigns[11].img;
 		document.getElementById("myZodiacSign").textContent=zodiacSigns[11].sign;
 		document.getElementById("myPosTraits").textContent="Your positive traits: " + zodiacSigns[11].positiveTraits;
 		document.getElementById("myNegTraits").textContent="Your negative traits: "+ zodiacSigns[11].negativeTraits;
