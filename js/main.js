555035e4f3d6c12b439a656714750a63be545065
Maribel <marleyva233@gmail.com>
2019-05-31 11:36:54 -0700

    typos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const zodiacSigns = {
 	Aries: {	
 		img: "img/newaries.png",
-		sign: "Aries",
 		positiveTraits:"Courageous, determined, confident, enthusiastic, optimistic, honest, passionate.",
 		negativeTraits: "Impatient, moody, short-tempered, impulsive, aggressive.",
 		loveMatch: "Libra, Leo",
@@ -9,7 +8,6 @@ const zodiacSigns = {
 	},
 	Taurus: {
 		img: "img/newtaurus.png",
-		sign: "Taurus",
 		positiveTraits: "Reliable, patient, practical, devoted, responsible, stable.",
 		negativeTraits: "Stubborn, possessive, uncompromising.",
 		loveMatch: "Scorpio or Cancer",
@@ -17,7 +15,6 @@ const zodiacSigns = {
 	},
 	Gemini: {
 		img: "img/newgemini.png",
-		sign: "Gemini",
 		positiveTraits: "Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas",
 		negativeTraits: "Nervous, inconsistent,  and indecisive.",
 		loveMatch: "Saggitarius, Aquarius",
@@ -25,7 +22,6 @@ const zodiacSigns = {
 	},
 	Cancer: {
 		img: "img/newcancer.png",
-		sign: "Cancer",
 		positiveTraits: "Tenacius, highly imaginative, loyal, emotional, sympathetic, persuasive.",
 		negativeTraits: "Moody, pessimistic, suspicious, manipulative, insecure.",
 		loveMatch: "Capricorn and Taurus.",
@@ -33,7 +29,6 @@ const zodiacSigns = {
 	},
 	Leo: {
 		img: "img/newleo.png",
-		sign: "Leo",
 		positiveTraits: "Creative, passionate, generous, warm-hearted, cheerful, humorous.",
 		negativeTraits: "Arrogant, stubborn, self-centered, lazy, inflexible",
 		loveMatch: "Aquarius and Gemini",
@@ -41,7 +36,6 @@ const zodiacSigns = {
 	},
 	Virgo: {
 		img: "img/newvirgo.png",
-		sign: "Virgo",
 		positiveTraits: "Loyal, analytical, kind, hardworking, practical.",
 		negativeTraits: "Shyness, worry, overly critical of self and others, all work and no play.",
 		loveMatch: "Pisces and Cancer.",
@@ -49,7 +43,6 @@ const zodiacSigns = {
 	},
 	Libra: {
 		img: "img/newlibra.png",
-		sign: "Libra",
 		positiveTraits: "Cooperative, diplomatic, gracious, fair-minded, social.",
 		negativeTraits: "indecisive, avoids confrontations, will carry a grudge, self-pity.",
 		loveMatch: "Aries and Saggitarius",
@@ -57,7 +50,6 @@ const zodiacSigns = {
 	},
 	Scorpio: {
 		img: "img/newscorpio.png",
-		sign: "Scorpio",
 		positiveTraits: "Resourceful, brave, passionate, stubborn, a true friend.",
 		negativeTraits: "Distrusting, jealous, secretive, violent.",
 		loveMatch: "Taurus and Cancer.",
@@ -65,7 +57,6 @@ const zodiacSigns = {
 	},
 	Sagittarius: {
 		img: "img/newsagittarius.png",
-		sign: "Sagittarius",
 		positiveTraits: "Generous, idealistic, great sense of humour.",
 		negativeTraits: "Promises more than can deliver, very impatient, will say anything no matter how undiplomatic.",
 		loveMatch: "Gemini and Aries.",
@@ -73,7 +64,6 @@ const zodiacSigns = {
 	},
 	Capricorn: {
 		img: "img/newcapricorn.png",
-		sign: "Capricorn",
 		positiveTraits: "responsible, disciplined, self-control, good manners.",
 		negativeTraits: "Know-it-all, unforgiving, condescending, expecting the worst.",
 		loveMatch: "Taurus and Cancer.",
@@ -81,14 +71,12 @@ const zodiacSigns = {
 	},
 	Aquarius: {
 		img: "img/newaquarius.png",
-		sign: "Aquarius",
 		positiveTraits: "Progressive, original, independent, humanitarian.",
 		negativeTraits:"Runs from emotional expression, temperamental, uncompromising, aloof.",
 		loveMatch: "Leo and Saggitarius.",
 		luckyDay: "Saturday",
 	},
 	Pisces: {
-		sign: "Pisces",
 		img: "img/newpisces.png",
 		positiveTraits: "Compassionate, artistic, intuitive, gentle, wise, musical.",
 		negativeTraits: "Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr.",
@@ -96,24 +84,23 @@ const zodiacSigns = {
 		luckyDay: "Thursday",
 	}
 }
- const select = document.getElementById("mySign");
- Object.entries(zodiacSigns).forEach(([key, val]) => {
-    let opt = document.createElement("option")
- 			opt.value = key
- 			opt.innerHTML = key
- 			select.appendChild(opt)
-});
+const select = document.getElementById("mySign");
+Object.entries(zodiacSigns).forEach(([key]) => {
+	    let opt = document.createElement("option")
+	 		opt.value = key;
+	 		opt.innerHTML = key;
+	 		select.appendChild(opt);
+	});
 function showMySign() {
     const selectedSign = document.getElementById("mySign").value;
-    console.log(selectedSign);
     const ball = document.getElementById("crystalball");
     	ball.src = zodiacSigns[selectedSign].img;
+    let signData = "<h1>" + selectedSign + "</h1><p>Your positive traits: " + zodiacSigns[selectedSign].positiveTraits + "</p>";
+    	signData += "<p>Your negative traits: " + zodiacSigns[selectedSign].negativeTraits + "</p>";
+    	signData += "<p>You get along best with: " + zodiacSigns[selectedSign].loveMatch + "</p>";
+    	signData += "<p>Your lucky day is: "+ zodiacSigns[selectedSign].luckyDay + "</p>";
     let output = document.getElementById("output");
     	output.style.display = "initial";
-    	output.innerHTML = "<h1>" + zodiacSigns[selectedSign].sign + "</h1>";
-    	output.innerHTML += "<p>Your positive traits: " + zodiacSigns[selectedSign].positiveTraits + "</p>";
-    	output.innerHTML += "<p>Your negative traits: " + zodiacSigns[selectedSign].negativeTraits + "</p>";
-    	output.innerHTML += "<p>You get along best with: " + zodiacSigns[selectedSign].loveMatch + "</p>";
-    	output.innerHTML += "<p>Your lucky day is: "+ zodiacSigns[selectedSign].luckyDay + "</p>";
+    	output.innerHTML = signData;
     	ball.classList.remove("flash");
 }
